4c3c9a2ec3e7a245f0304da1376fdffd0a4f8a90
Bogdan74T <bgdntanase@yahoo.com>
2023-06-30 23:30:15 +0300

    first commit

---
+++ api_clients.py
@@ -0,0 +1,20 @@
+from random import randint
+import requests
+
+def login(clientName=None,clientEmail=None):
+    body = {
+        'clientName': ClientName,
+        'clientEmail': clientEmail
+    }
+    response = requests.post('https://simple-books-api.glitch.me/api-clients', json=body)
+    return response
+
+def get_token():
+    nr = randint(1, 9999999)
+    json = {
+        'clientName': 'Bogdan',
+        'clientEmail': f'valid_email_test{nr}@mailinator.com'
+    }
+
+    response = requests.post('https://simple-books-api.glitch.me/api-clients', json=json)
+    return response.json()['accessToken']
